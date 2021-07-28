22c25e8b126c2c866d87fff8d16f65e7777fe613
svmatyushin2 <87603631+svmatyushin2@users.noreply.github.com>
2021-07-28 20:51:04 +0300

    class_1_5

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,6 +5,11 @@ import jdk.swing.interop.SwingInterOpUtils;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		String somebody = "world";
+		System.out.println("Hello, " +somebody+"!");
+
+		double l = 8.0;
+		double s = l * l;
+		System.out.println("Площаь квадрата со стороной "+ l + " = " + s);
 	}
 }
