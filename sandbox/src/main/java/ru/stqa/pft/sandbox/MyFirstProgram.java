a2a729e4c810c10de9c2391d9062c752dd0dcd88
svmatyushin2 <87603631+svmatyushin2@users.noreply.github.com>
2021-08-02 21:04:17 +0300

    Evenint 02082021

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,23 +5,17 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		//System.out.println("Hello, world!");
 		hello("world");
-		double len = 5;
-		System.out.println("Площадь квадрата со стороной " +len +" = " +area(len));
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника = " +area(a, b));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " +s.l +" = " +s.area());
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = " +r.area());
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " +somebody+ "!");
 	}
 
-	public static double area(double l) {
-		return  l*l;
-	}
 
-	public static double area(double a, double b) {
-		return a * b;
-	}
 }
